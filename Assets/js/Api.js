226c59c8af4b78a5d3c607b64a1873d2776b64f0
Oaksas <aman.getnet2@gmail.com>
2021-02-25 17:08:11 -1100

    Start working on NASA API TEST

---
+++ Assets/js/Api.js
@@ -0,0 +1,96 @@
+const imageView = document.getElementById("imgView")
+const titleView = document.getElementById("titleView")
+const descriptionView = document.getElementById("descriptionView")
+const data = document.getElementById('data');
+
+loadDataNew();
+
+async function sendRequest(){
+    let api_key = "weAlGOG143bhhKcNAnzlIo64MUoJwLy9GcD17J9K";
+    let request = await fetch("https://images-api.nasa.gov/search?media_type=image&q=mars");
+    let data= await  request.json();
+    let d = data.collection.items;
+    return d;
+}
+function loadDataNew() {
+    
+    sendRequest().then(
+        function(posts) {
+        //iterate over each post [100 posts]
+        let output = " ";
+        posts.forEach(x => {
+            console.log(x);
+
+            
+            output += `
+            <div class="col-6">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <img src=" ${x.links[0].href}">
+                  <h2 class="card-title">${x.data[0].title}</h2>
+
+                  <h5 class="card-title">${x.data[0].description}</h5>
+
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>    
+`
+        });
+
+        data.innerHTML = output;
+  })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+}
+
+
+// function load_fromPlaceHolder() {
+//     var apiSource = "https://api.nasa.gov/planetary/apod?api_key=weAlGOG143bhhKcNAnzlIo64MUoJwLy9GcD17J9K"
+
+//     //open the request 
+//     fetch(apiSource)
+//         .then(function(res) {
+//             return res.json(); //return the JSON Promise
+//         })
+//         .then(function(posts) {
+//             //iterate over each post [100 posts]
+//             let output = '';
+//             posts.forEach(function(post) {
+//                 titleView.innerHTML = post.title;
+//                 descriptionView.innerHTML = post.explanation;
+//                 imageView.scr = post.url;
+
+//         //         output += `
+        
+//         //         <div class="item">
+//         //         <div class="image">
+//         //             <img src="${post.img}">
+//         //         </div>
+//         //         <div class="content">
+//         //             <a class="header" href="#" id="bTitle">
+//         //             ${post.title.toUpperCase()}
+//         //             </a>
+//         //             <div class="description">
+//         //                 <p id="bDesc">
+//         //                 ${post.explanation}
+//         //                 </p>
+//         //             </div>
+//         //             <div class="extra">
+//         //                 <a class="ui floated basic violet button" href="#">Read Mores</a>
+//         //             </div>
+//         //         </div>
+//         //     </div>
+//         // `;
+//             });
+
+//         })
+//         .catch(function(err) {
+//             console.log(err);
+//         });
+
+
+
+// }
